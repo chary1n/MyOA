c353964b9399aae3901a1875a718dd44bfc8e2cd
bin <bin@bindeMacBook-Pro.local>
2017-07-19 17:52:52 +0800

    提交

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,8 @@ export class MyApp {
       
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleBlackTranslucent();
+      
+      statusBar.show();
       splashScreen.hide();
     });
   }
