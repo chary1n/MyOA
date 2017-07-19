47283f7c18f900ef721fdc3f3be6a86bafacf129
bin <bin@bindeMacBook-Pro.local>
2017-07-19 19:48:46 +0800

    statusbar修改

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,14 +21,11 @@ export class MyApp {
       
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      document.addEventListener('deviceready', onDeviceReady, false);
+      statusBar.overlaysWebView(false);
+      statusBar.styleBlackTranslucent();
       splashScreen.hide();
     });
   }
   
 }
-function onDeviceReady() {  
-    // if (parseFloat(window.device.version) >= 7.0) {  
-          document.body.style.marginTop = "20px";  
-    // }  
-}
+
