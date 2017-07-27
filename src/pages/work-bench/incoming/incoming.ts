b1c99611c76645a56f4db47d023c3e1e7cb0b682
bin <bin@bindeMacBook-Pro.local>
2017-07-27 13:26:22 +0800

    冲突

--- src/pages/work-bench/incoming/incoming.ts
+++ src/pages/work-bench/incoming/incoming.ts
@@ -1,9 +1,9 @@
-// import { IncomingService } from './incomingService';
+import { IncomingService } from './incomingService';
 import { APK_DOWNLOAD } from './../../../providers/Constants';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { StockTransfer } from './../../../model/StockTransferModel';
-import { IncomingService } from './incomingService';
+
+
 /**
  * Generated class for the IncomingPage page.
  *
@@ -14,36 +14,25 @@ import { IncomingService } from './incomingService';
 @Component({
   selector: 'page-incoming',
   templateUrl: 'incoming.html',
-  providers: [IncomingService],
+  providers: [IncomingService]
 })
 export class IncomingPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams,private incomingService: IncomingService) {
-// =======
-//   providers: [IncomingService]
-// })
-// export class IncomingPage {
   
-//   items : any;
-//   limit = 20 ;
-//   offset = 0 ;
-//   constructor(public navCtrl: NavController, public navParams: NavParams,
-//       public incomingService :IncomingService) {
-// >>>>>>> 647901ff4eb547d4a02fab3fae2279ddd69f96fa
-//   }
+  items : any;
+  limit = 20 ;
+  offset = 0 ;
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+      public incomingService :IncomingService) {
+  }
 
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IncomingPage');
-// <<<<<<< HEAD
-    this.incomingService.getStockList(0,1,'validate',20,0);
-// =======
-//     this.incomingService.getIncomingList(this.limit,this.offset).then((res)=>{
-//       console.log(res)
-//       this.items=res.result.res_data;
-//     })
-// >>>>>>> 647901ff4eb547d4a02fab3fae2279ddd69f96fa
-//   }
+    // this.incomingService.getIncomingList(this.limit,this.offset).then((res)=>{
+    //   console.log(res)
+    //   this.items=res.result.res_data;
+    // })
+  }
 
   doRefresh(refresh) {
     refresh.complete();
--- src/pages/work-bench/incoming/incomingService.ts
+++ src/pages/work-bench/incoming/incomingService.ts
@@ -1,23 +1,6 @@
 import { HttpService } from './../../../providers/HttpService';
 import { Injectable } from '@angular/core';
 
-<<<<<<< HEAD
-@Injectable()
-export class IncomingService {
-    constructor(private httpService: HttpService) {
-
-    }
-
-    getStockList(partner_id,picking_type_id,state,limit,offset){
-        let body = JSON.stringify({
-            partner_id: partner_id,
-            picking_type_id: picking_type_id,
-            state: state,
-            limit:limit,
-            offset:offset,
-        });
-        return this.httpService.get('get_stock_picking_list', body,1);
-=======
 
 @Injectable()
 export class IncomingService {
@@ -34,7 +17,6 @@ export class IncomingService {
             offset:moffset
         });
        return this.httpservice.postBody("get_stock_picking_list",body,1);
->>>>>>> 647901ff4eb547d4a02fab3fae2279ddd69f96fa
     }
 
 }
