cedb45b375d5e3b6ac4159075680e96e871d4a59
bin <bin@bindeMacBook-Pro.local>
2017-07-28 16:49:16 +0800

    入库详情页面

--- src/pages/work-bench/incoming/incoming.ts
+++ src/pages/work-bench/incoming/incoming.ts
@@ -3,7 +3,7 @@ import { APK_DOWNLOAD } from './../../../providers/Constants';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { IncomingDetailPage} from './../incoming-detail/incoming-detail'
-
+import { LoadingController } from 'ionic-angular';
 /**
  * Generated class for the IncomingPage page.
  *
@@ -22,14 +22,20 @@ export class IncomingPage {
   limit = 20 ;
   offset = 0 ;
   constructor(public navCtrl: NavController, public navParams: NavParams,
-      public incomingService :IncomingService) {
+      public incomingService :IncomingService , public loadingCtrl: LoadingController) {
   }
 
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IncomingPage');
+   let load = this.loadingCtrl.create({
+      content: '加载中...',
+      dismissOnPageChange: true
+    });
+    load.present();
      this.incomingService.getIncomingList(this.limit,this.offset).then((res)=>{
        console.log(res)
+       load.dismiss();
        this.items=res.result.res_data;
      })
   }
@@ -43,7 +49,10 @@ export class IncomingPage {
 
   }
 
-incoming_detail(){
+incoming_detail(item){
+  this.navCtrl.push(IncomingDetailPage,{
+      item:item,
+  });
 }
 
 }
