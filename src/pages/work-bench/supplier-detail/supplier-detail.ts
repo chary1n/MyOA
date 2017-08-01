452ad5b5f57f88923fd8f6d261564ff58e086f03
shangdinvxu <997399759@qq.com>
2017-08-01 17:30:57 +0800

    figures

--- src/pages/work-bench/supplier-detail/supplier-detail.ts
+++ src/pages/work-bench/supplier-detail/supplier-detail.ts
@@ -10,6 +10,8 @@ import { CallNumber } from '@ionic-native/call-number';
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
+
+ 
 @IonicPage()
 @Component({
   selector: 'page-supplier-detail',
