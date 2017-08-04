f8bde942cabfa31ecb876bfc6f9bcd03add5001b
shangdinvxu <997399759@qq.com>
2017-08-04 17:54:34 +0800

    tijiao

--- src/providers/Constants.ts
+++ src/providers/Constants.ts
@@ -1,5 +1,5 @@
 // export const APP_SERVER_URL= 'http://192.168.2.148:8069/';
-export const APP_SERVER_URL= 'http://192.168.2.148:8069/';
+export const APP_SERVER_URL= 'http://erp.robotime.com/';
 export const APP_SERVER_URL_T= 'http://erp.robotime.com/';
 export const APPSUBPATH = 'linkloving_app_api/';
 export const OAUBPATH = 'linkloving_oa_api/';
