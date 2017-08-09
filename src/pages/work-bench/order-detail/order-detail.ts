f7b20f01d2a4252863dfe1bbc080952aa4121a75
shangdinvxu <997399759@qq.com>
2017-08-09 09:46:38 +0800

    提交显示和隐藏数量

--- src/pages/work-bench/order-detail/order-detail.ts
+++ src/pages/work-bench/order-detail/order-detail.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { IonicPage, NavController, NavParams,PopoverController ,ViewController,Events} from 'ionic-angular';
+import { IonicPage, NavController, NavParams, PopoverController, ViewController, Events } from 'ionic-angular';
 import { orderService } from '../order/orderService';
 import { PoContactPage } from './../po-contact/po-contact';
 import { DeliveryNotesPage } from './../delivery-notes/delivery-notes';
@@ -11,32 +11,29 @@ import { DeliveryNotesPage } from './../delivery-notes/delivery-notes';
       <button ion-item tappable (click)="delivery()">送货</button>
     </ion-list>
   `,
-  providers: [orderService,PoContactPage,DeliveryNotesPage]
+  providers: [orderService, PoContactPage, DeliveryNotesPage]
 })
 export class PopoverPage {
-  id:any;
-  constructor(public viewCtrl: ViewController,public orderService:orderService,public pocontactCtrl:PoContactPage,public deliveryCtrl:DeliveryNotesPage,public events: Events) {
+  id: any;
+  constructor(public viewCtrl: ViewController, public orderService: orderService, public pocontactCtrl: PoContactPage, public deliveryCtrl: DeliveryNotesPage, public events: Events) {
     this.id = viewCtrl.getNavParams().get('id');
-    
+
   }
   close() {
     this.viewCtrl.dismiss();
   }
-  click_phone()
-  {
-    this.orderService.get_contact_phone_number(this.id,"purchase.order").then((res) => {
-        let item_detai = res.result.res_data;      
-        if (item_detai)
-        {
-          this.events.publish('click:purchase.order', item_detai);  
-        }
+  click_phone() {
+    this.orderService.get_contact_phone_number(this.id, "purchase.order").then((res) => {
+      let item_detai = res.result.res_data;
+      if (item_detai) {
+        this.events.publish('click:purchase.order', item_detai);
+      }
     })
   }
-  delivery(){
+  delivery() {
     this.orderService.get_delivery_notes(this.id).then((res) => {
       let item_detai = res.result.res_data;
-      if (item_detai)
-      {
+      if (item_detai) {
         this.events.publish('delivery', item_detai);
       }
     })
@@ -48,53 +45,54 @@ export class PopoverPage {
 @Component({
   selector: 'page-order-detail',
   templateUrl: 'order-detail.html',
-  
+
 })
 export class OrderDetailPage {
-  
+
   @ViewChild('content', { read: ElementRef }) content: ElementRef;
   @ViewChild('popoverText', { read: ElementRef }) text: ElementRef;
-  item :any 
-  popover:any
-  constructor(public navCtrl: NavController, public navParams: NavParams,public popoverCtrl: PopoverController,public events:Events) {
-   this.item =navParams.get('item').res_data
-   console.log(this.item)
-   
+  item: any
+  popover: any
+  showNumber: boolean
+  constructor(public navCtrl: NavController, public navParams: NavParams, public popoverCtrl: PopoverController, public events: Events) {
+    this.item = navParams.get('item').res_data
+    this.showNumber = navParams.get('showNumber');
+    console.log(this.item)
   }
 
-  
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrderDetailPage');
   }
-  ionViewDidEnter(){
+  ionViewDidEnter() {
     this.events.unsubscribe('click:purchase.order');
     this.events.unsubscribe('delivery');
     this.events.subscribe('click:purchase.order', (eventData) => {
       this.navCtrl.push(PoContactPage, {
-             items: eventData
-           })
+        items: eventData
+      })
       this.events.unsubscribe('click:purchase.order');
-      this.popover.dismiss();     
+      this.popover.dismiss();
     });
 
     this.events.subscribe('delivery', (eventData) => {
       this.navCtrl.push(DeliveryNotesPage, {
-            items: eventData,
-            type: "purchase"
-          })
+        items: eventData,
+        type: "purchase"
+      })
       this.events.unsubscribe('delivery');
-      this.popover.dismiss();     
+      this.popover.dismiss();
     });
   }
-  
+
 
   presentPopover(ev) {
-    
+
     this.popover = this.popoverCtrl.create(PopoverPage, {
-         id:this.item.id
+      id: this.item.id
     });
-    
+
     this.popover.present({
       ev: ev
     });
