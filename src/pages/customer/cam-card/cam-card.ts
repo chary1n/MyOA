fed24e4b8c48018d6a5b6241163d949aa3727ec2
shangdinvxu <997399759@qq.com>
2017-08-23 15:48:37 +0800

    保存

--- src/pages/customer/cam-card/cam-card.ts
+++ src/pages/customer/cam-card/cam-card.ts
@@ -44,10 +44,9 @@ export class CamCardPage {
         if (res != null) {
           if (res.result.res_data.team)
           {
-            this.saleteam_id = res.result.res_data.team.team_id ? res.result.res_data.team.team_id : '';
+          this.saleteam_id = res.result.res_data.team.team_id ? res.result.res_data.team.team_id : '';
           this.saleteam_name = res.result.res_data.team.team_name ? res.result.res_data.team.team_name : '';
           }
-          
           this.saleman_id = res.result.res_data.user_id;
           this.saleman_name = res.result.res_data.name;
         } 
