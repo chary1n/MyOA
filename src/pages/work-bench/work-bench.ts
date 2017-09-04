832ffed8be2a299f35bd5bd048158b899612f404
shangdinvxu <997399759@qq.com>
2017-09-04 17:32:27 +0800

    工作台界面完成之后的提交

--- src/pages/work-bench/work-bench.ts
+++ src/pages/work-bench/work-bench.ts
@@ -28,67 +28,57 @@ import { BiaoQianPage } from './../customer/biao-qian/biao-qian';
 export class WorkBenchPage {
   dataSource: any
   model: WorkBenchModel
+  isShowPurchase = false;
+  isShowSale = false;
+  isHR = false ;
   constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
-
-
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad WorkBenchPage');
-
   }
 
   ionViewDidEnter() {
-    let isShowPurchase = false;
-    let isShowSale = false;
+
     this.storage.get('user')
       .then(res => {
         console.log(res);
         for (let product of res.result.res_data.groups) {
           if (product.name == 'group_purchase_user' || product.name == 'group_purchase_manager') {
-            isShowPurchase = true;
+            this.isShowPurchase = true;
           }
           if (product.name == 'group_sale_salesman' || product.name == 'group_sale_manager' || product.name == 'group_sale_salesman_all_leads') {
-            isShowSale = true;
+            this.isShowSale = true;
           }
         }
-        let newArr = [];
-        if (isShowPurchase) {
-          newArr.push(new WorkBenchModel('assets/img/shenpi.png', '采购入库'));
-          newArr.push(new WorkBenchModel('assets/img/shenpi.png', '供应商'));
-          newArr.push(new WorkBenchModel('assets/img/shenpi.png', '采购订单'));
-        }
-        if (isShowSale) {
-          newArr.push(new WorkBenchModel('assets/img/shenpi.png', '客户查询'));
-          newArr.push(new WorkBenchModel('assets/img/shenpi.png', '产品查询'));
-          newArr.push(new WorkBenchModel('assets/img/shenpi.png', '销售订单'));
-          newArr.push(new WorkBenchModel('assets/img/mingpian.png', '名片扫描'));
-        }
-        this.dataSource = newArr;
       });
   }
 
-  click(item) {
-    if (item.title == '采购入库') {
-      this.navCtrl.push(IncomingPage);
-    }
-    else if (item.title == '供应商') {
-      this.navCtrl.push(SupplierListPage);
-    }
-    else if (item.title == '采购订单') {
-      this.navCtrl.push(OrderPage);
-    } else if (item.title == '销售订单') {
-      this.navCtrl.push(SalesOrderPage);
-    }
-    else if (item.title == '客户查询') {
-      this.navCtrl.push(CustomerPage);
-    }
-    else if (item.title == '名片扫描') {
-      this.navCtrl.push(CamCardPage);
-    }
-    else if (item.title == '产品查询') {
-      this.navCtrl.push(ProductionSearchPage);
-    }
+  clickInComingWareHouse() {
+    this.navCtrl.push(IncomingPage);
+  }
+  supplierList() {
+    this.navCtrl.push(SupplierListPage);
+  }
+
+  purchaseOrder() {
+    this.navCtrl.push(OrderPage);
+  }
+
+  salesOrder() {
+    this.navCtrl.push(SalesOrderPage);
+  }
+
+  customerSearch() {
+    this.navCtrl.push(CustomerPage);
+  }
+
+  camCard() {
+    this.navCtrl.push(CamCardPage);
+  }
+
+  ProductionSearch() {
+    this.navCtrl.push(ProductionSearchPage);
   }
 
 }
