7928cb9c5499927a8cbd65bb576863484d22ef3a
shangdinvxu <997399759@qq.com>
2017-10-17 09:47:35 +0800

    更改的提交

--- src/pages/work-bench/apply/apply.ts
+++ src/pages/work-bench/apply/apply.ts
@@ -2,9 +2,8 @@ import { CreateApplyPage } from './../create-apply/create-apply';
 import { ApplyDetailPage } from './../apply-detail/apply-detail';
 import { Storage } from '@ionic/storage';
 import { CommonUseServices } from './../commonUseServices';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
 import { Component } from '@angular/core';
-import { AlertController } from 'ionic-angular';
 
 
 /**
@@ -20,82 +19,73 @@ import { AlertController } from 'ionic-angular';
   providers: [CommonUseServices],
 })
 export class ApplyPage {
-  applyList :any ;
-  user ;
+  applyList: any;
+  user;
   user_id;
+  actionSheetCtrl;
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    public commonService :CommonUseServices,public storage : Storage,
-    public alertCtrl: AlertController) {
-      
+    public commonService: CommonUseServices, public storage: Storage,
+    actionSheetCtrl: ActionSheetController) {
+      this.actionSheetCtrl = actionSheetCtrl
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ApplyPage');
     this.storage.get('user')
-    .then(res => {
-      this.user_id = res.result.res_data.user_id;
-      console.log(this.user_id);
-      this.getApplyList(20,0,this.user_id)
-    });
+      .then(res => {
+        this.user_id = res.result.res_data.user_id;
+        console.log(this.user_id);
+        this.getApplyList(20, 0, this.user_id)
+      });
   }
-/**
- * 
- * create_time:"2017-10-11 07:37:23"
-department:"董事长 / 总经理wiliam / 工程部"
-id:2505
-name:"BX1710112075"
-payment:10
-state:"submit"
-*/
-  getApplyList(limit,offset,id){
-    this.commonService.getApplyList(offset,limit,id).then(res=>{
-      if(res.result && res.result.res_code == 1){
-       this.applyList = res.result.res_data;
+
+  getApplyList(limit, offset, id) {
+    this.commonService.getApplyList(offset, limit, id).then(res => {
+      if (res.result && res.result.res_code == 1) {
+        this.applyList = res.result.res_data;
       }
     })
   }
 
-  showRadio() {
-    let alert = this.alertCtrl.create();
-    alert.setTitle('请选择类型');
-    alert.addInput({
-        type: 'radio',
-        label: '报销',
-        value: '1',
-        checked: true
-    });
-    alert.addInput({
-      type: 'radio',
-      label: '申购',
-      value: '2',
-      checked: false
-  });
-  alert.addInput({
-    type: 'radio',
-    label: '请假',
-    value: '3',
-    checked: false
-});
-    alert.addButton('取消');
-    alert.addButton({
-        text: '确定',
-        handler: data => {
-        this.navCtrl.push('CreateApplyPage');
+  showActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: '请选择类型',
+      buttons: [
+        {
+          text: '报销',
+          handler: () => {
+          }
+        },{
+          text: '申购',
+          handler: () => {
+          }
+        },{
+          text: '请假',
+          handler: () => {
+          }
+        },{
+          text: '取消',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
         }
+      ]
     });
-    alert.present();
-}
+    actionSheet.present();
+  } 
 
-clickApply(id){
-  this.commonService.getApplyDetail(id).then(res=>{
-    if(res.result && res.result.res_code == 1){
+  clickApply(id) {
+    this.commonService.getApplyDetail(id).then(res => {
+      if (res.result && res.result.res_code == 1) {
         console.log(res);
-        this.navCtrl.push('ApplyDetailPage',{
-          res_data : res.result.res_data
+        this.navCtrl.push('ApplyDetailPage', {
+          res_data: res.result.res_data
         });
-    }
-  })
+      }
+    })
 
-}
+  }
 
 }
