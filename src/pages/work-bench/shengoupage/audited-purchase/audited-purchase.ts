ade3c761954de6c9306855b7745f550cbd5418d0
shangdinvxu <997399759@qq.com>
2017-11-08 15:34:06 +0800

    审核

---
+++ src/pages/work-bench/shengoupage/audited-purchase/audited-purchase.ts
@@ -0,0 +1,166 @@
+import { Storage } from '@ionic/storage';
+import { Utils } from './../../../../providers/Utils';
+import { ShenGouService } from './../shengouService';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the AuditedPurchasePage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-audited-purchase',
+  templateUrl: 'audited-purchase.html',
+  providers: [ShenGouService]
+})
+export class AuditedPurchasePage {
+
+  item: any;
+  title: any;
+  isShowFooter: any;
+  user_id: any;
+  frontPage;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public shengouService: ShenGouService,
+    public alertCtrl: AlertController, public storage: Storage) {
+    this.item = this.navParams.get('item');
+    this.title = this.item.name;
+    this.frontPage = Utils.getViewController("ShengoupagePage", navCtrl)
+    this.storage.get('user')
+      .then(res => {
+        console.log(res)
+        this.user_id = res.result.res_data.user_id;
+      });
+    console.log(this.item.state);
+    if (this.item.state == 'submit') {
+      this.isShowFooter = true;
+    }
+    else {
+      this.isShowFooter = false;
+    }
+    console.log(this.item)
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ReimbursementDetailPage');
+  }
+  
+
+  conform() {
+    let ctrl = this.alertCtrl;
+      ctrl.create({
+        title: '提示',
+        subTitle: "确定审核通过?",
+        buttons: [
+        {
+          text: '取消',
+          handler: () => {
+          }
+        },
+        {
+          text: '确定',
+          handler: () => {
+            this.submit()
+          }
+        }
+        ],
+      }).present();
+  }
+  submit(){
+    let ctrl = this.alertCtrl;
+    if (this.item.state == 'submit') {
+      this.shengouService.confirm1(this.item.sheet_id, this.user_id).then((res) => {
+        if (res) {
+          if (res.result.res_data.success == 1) {
+            console.log(res.result.res_data.success)
+            ctrl.create({
+              title: '提示',
+              subTitle: "审批成功",
+              buttons: [{
+                text: '确定',
+                handler: () => {
+                  this.frontPage.data.need_fresh = true;
+                  this.navCtrl.popTo(this.frontPage, {
+                    need_fresh: true,
+                  });
+                }
+              }
+              ]
+            }).present();
+          }
+        }
+      })
+    }
+  }
+
+  cancel() {
+    this.showPrompt();
+  }
+
+  showPrompt() {
+    let ctrl = this.alertCtrl;
+    ctrl.create({
+      title: '提示',
+      message: "输入拒绝的原因",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: '拒绝原因'
+        },
+      ],
+      buttons: [
+        {
+          text: '取消',
+          handler: data => {
+            // console.log('Cancel clicked');
+          }
+        },
+        {
+          text: '确定',
+          handler: data => {
+            if (data.title) {
+              this.shengouService.refuse(this.item.sheet_id, data.title, this.user_id).then((res) => {
+                if (res) {
+
+                  if (res.result.res_data.success == 1) {
+                    console.log(res.result.res_data.success)
+                    ctrl.create({
+                      title: '提示',
+                      subTitle: "审批成功",
+                      buttons: [{
+                        text: '确定',
+                        handler: () => {
+                          this.frontPage.data.need_fresh = true;
+                          this.navCtrl.popTo(this.frontPage, {
+                            need_fresh: true,
+                          });
+                        }
+                      }
+                      ]
+                    }).present();
+                  }
+                }
+              })
+            }
+            else {
+              ctrl.create({
+                title: '提示',
+                subTitle: "请填写拒绝原因",
+                buttons: [{
+                  text: '确定',
+                  handler: () => {
+
+                  }
+                }
+                ]
+              }).present();
+            }
+          }
+        }
+      ]
+    }).present();
+  }
+
+}
