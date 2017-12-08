89bef5ed1ff4579411f6cebfb00f1c7da48c1854
shangdinvxu <997399759@qq.com>
2017-12-08 16:36:21 +0800

    打包

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -110,8 +110,8 @@ export class LoginPage {
     this.isSelected1 = true;
     this.isSelected2 = false;
     this.isSelected3 = false;
-    // HttpService.appUrl = "http://js.robotime.com/"
-    HttpService.appUrl = "http://192.168.88.135:8069/"
+    HttpService.appUrl = "http://js.robotime.com/"
+    // HttpService.appUrl = "http://192.168.88.135:8069/"
     this.getDB();
   }
 
@@ -127,8 +127,8 @@ export class LoginPage {
     this.isSelected3 = true;
     this.isSelected2 = false;
     this.isSelected1 = false;
-    // HttpService.appUrl = "http://erp.robotime.com/"
-    HttpService.appUrl = "http://192.168.2.38:8111/"
+    HttpService.appUrl = "http://erp.robotime.com/"
+    // HttpService.appUrl = "http://192.168.2.38:8111/"
     this.getDB();
   }
 
