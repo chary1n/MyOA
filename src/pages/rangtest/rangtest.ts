01ec039d7570b3d1121ab1225ddd40b83d6ade22
shangdinvxu <997399759@qq.com>
2018-01-16 16:50:29 +0800

    保存

--- src/pages/rangtest/rangtest.ts
+++ src/pages/rangtest/rangtest.ts
@@ -27,37 +27,8 @@ export class RangtestPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RangtestPage');
-    this.loop()
   }
 
-  loop() {
-    var canvas = <HTMLCanvasElement>document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    canvas.width = 200 ;
-    canvas.height = 80;
-    var boHeight = canvas.height / 10;
-    var posHeight = canvas.height / 1.2;
-    var lines = ["rgba(0,222,255, 0.8)"];
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (this.step == 80) {
-      this.step = 0;
-    }
-    this.step = this.step + 20;
-    for (var j = lines.length - 1; j >= 0; j--) {
-      ctx.fillStyle = lines[j];
-      var angle = (this.step + j * 50) * Math.PI / 180;
-      var deltaHeight = Math.sin(angle) * boHeight;
-      var deltaHeightRight = Math.cos(angle) * boHeight;
-      ctx.beginPath();
-      ctx.moveTo(0, posHeight + deltaHeight);
-      ctx.bezierCurveTo(canvas.width / 2, posHeight + deltaHeight - boHeight, canvas.width / 2, posHeight + deltaHeightRight - boHeight, canvas.width, posHeight + deltaHeightRight);
-      ctx.lineTo(canvas.width, canvas.height);
-      ctx.lineTo(0, canvas.height);
-      ctx.lineTo(0, posHeight + deltaHeight);
-      ctx.closePath();
-      ctx.fill();
-    }
-  }
+  
 
 }
--- src/pages/work-bench/gongdan/gongdan.ts
+++ src/pages/work-bench/gongdan/gongdan.ts
@@ -40,7 +40,6 @@ export class GongdanPage {
     console.log('ionViewDidLoad GongdanPage');
     this.statusbar.backgroundColorByHexString("#2597ec");
     this.statusbar.styleLightContent();
-    this.loop();
     // this.step = 1;
     // this.loop();
     // window.setInterval(() => {
@@ -50,7 +49,7 @@ export class GongdanPage {
 
   click_me() {
     this.show_type = "me"
-    this.loop();
+    this.looper();
     this.gongdanService.my_work_order_statistics().then(res => {
       if (res.result && res.result.res_code == 1) {
         this.processNumber = res.result.res_data.process
@@ -96,34 +95,46 @@ export class GongdanPage {
   }
 
 
-  loop() {
+  looper() {
     var canvas = <HTMLCanvasElement>document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
-    canvas.height = 80;
+    canvas.width = 300;
+    canvas.height = 100;
+    //如果浏览器支持requestAnimFrame则使用requestAnimFrame否则使用setTimeout  
+    function requestAnimFrame(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+    // 波浪大小
     var boHeight = canvas.height / 10;
     var posHeight = canvas.height / 1.2;
-    var lines = ["rgba(0,222,255, 0.2)"];
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (this.step == 80) {
-      this.step = 0;
+    //初始角度为0  
+    var step = 0;
+    //定义三条不同波浪的颜色  
+    var lines = [
+      "rgba(0,255,255, 0.2)",
+      "rgba(200,236,253, 0.2)"];
+    function loop() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      step++;
+      //画3个不同颜色的矩形  
+      for (var j = lines.length - 1; j >= 0; j--) {
+        ctx.fillStyle = lines[j];
+        //每个矩形的角度都不同，每个之间相差45度  
+        var angle = (step + j * 50) * Math.PI / 180;
+        var deltaHeight = Math.sin(angle) * boHeight;
+        var deltaHeightRight = Math.cos(angle) * boHeight;
+        ctx.beginPath();
+        ctx.moveTo(0, posHeight + deltaHeight);
+        ctx.bezierCurveTo(canvas.width / 2, posHeight + deltaHeight - boHeight, canvas.width / 2, posHeight + deltaHeightRight - boHeight, canvas.width, posHeight + deltaHeightRight);
+        ctx.lineTo(canvas.width, canvas.height);
+        ctx.lineTo(0, canvas.height);
+        ctx.lineTo(0, posHeight + deltaHeight);
+        ctx.closePath();
+        ctx.fill();
+      }
+      requestAnimFrame(loop);
     }
-    this.step = this.step + 20;
-
-    for (var j = lines.length - 1; j >= 0; j--) {
-      ctx.fillStyle = lines[j];
-      var angle = (this.step + j * 50) * Math.PI / 180;
-      var deltaHeight = Math.sin(angle) * boHeight;
-      var deltaHeightRight = Math.cos(angle) * boHeight;
-      ctx.beginPath();
-      ctx.moveTo(0, posHeight + deltaHeight);
-      ctx.bezierCurveTo(canvas.width / 2, posHeight + deltaHeight - boHeight, canvas.width / 2, posHeight + deltaHeightRight - boHeight, canvas.width, posHeight + deltaHeightRight);
-      ctx.lineTo(canvas.width, canvas.height);
-      ctx.lineTo(0, canvas.height);
-      ctx.lineTo(0, posHeight + deltaHeight);
-      ctx.closePath();
-      ctx.fill();
-    }
+    loop();
   }
   goBack() {
     this.statusbar.backgroundColorByHexString("#ffffff");
