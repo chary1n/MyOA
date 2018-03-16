337632c90539c9a78b8d6c77c2ed0cd5827013cf
PeaceP <747780958@qq.com>
2018-03-16 13:22:00 +0800

    考勤提交

--- src/pages/work-bench/kaoqin/kaoqinService.ts
+++ src/pages/work-bench/kaoqin/kaoqinService.ts
@@ -20,6 +20,27 @@ export class KaoQinService {
     employee_attendance(data_params)
     {
         let body = JSON.stringify(data_params);
-       return this.httpservice.postBody("employee_attendance",body);
+       return this.httpservice.postBodyNoLoading("employee_attendance",body);
+    }
+
+    get_ble_device(){
+        let body = JSON.stringify({});
+       return this.httpservice.postBodyNoLoading("get_ble_device",body);
+    }
+
+    get_employee_attendance(day_start,day_end,user_id){
+        let body = JSON.stringify({
+            day_start:day_start,
+            day_end:day_end,
+            user_id:user_id,
+        });
+       return this.httpservice.postBodyNoLoading("get_employee_attendance",body);
+    }
+
+    get_is_department(employee_id){
+        let body = JSON.stringify({
+            employee_id:employee_id,
+        });
+       return this.httpservice.postBodyNoLoading("get_is_department",body);
     }
 }
