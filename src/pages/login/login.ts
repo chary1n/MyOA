adbc8d4ab255a092e2855e3c5e01037a42c80eab
PeaceP <747780958@qq.com>
2018-03-20 16:42:00 +0800

    login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -178,9 +178,9 @@ export class LoginPage {
     this.isSelected1 = false;
     this.isSelected4 = false;
     this.chooseIndex = 3;
-    HttpService.appUrl = "http://erp.robotime.com/"
-    // HttpService.appUrl = "http://192.168.88.131:8069/"
-    HttpService.appUrl = "http://192.168.2.34:8888/"   
+    // HttpService.appUrl = "http://erp.robotime.com/"
+    HttpService.appUrl = "http://192.168.88.131:8069/"
+    // HttpService.appUrl = "http://192.168.2.34:8888/"   
     this.reset();
     this.img3 = "assets/img/ruobeier_clicked.png" 
     this.password_src="assets/img/R_password.png"
