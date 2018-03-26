efdb1cf457de3bfbd058cc8c3323a18d7c3073a5
zouwansheng <1750639804@qq.com>
2018-03-26 08:35:45 +0800

    提交

--- src/pages/work-bench/work-bench.ts
+++ src/pages/work-bench/work-bench.ts
@@ -18,10 +18,11 @@ import { StatusBar } from '@ionic-native/status-bar';
   providers:[CommonUseServices],
 })
 export class WorkBenchPage {
-  isSale;
-  isBuy;
-  isEngineer;
-  isMoney;
+  isSale = false;//销售
+  isBuy = false;//采购
+  isEngineer;//工程
+  isMoney;//财务
+  currenTab;//目前所在的tab
   dataSource: any
   model: WorkBenchModel
   isShowPurchase = false;
@@ -65,19 +66,30 @@ export class WorkBenchPage {
         if(res.result.res_data.team){
           this.isShowJournalSheet = true
         }
+        
         for (let product of res.result.res_data.groups) {
           if(product.name == 'group_inventory_user'){
             this.isShowKucun = true;
           }
           if (product.name == 'group_sale_salesman' || product.name == 'group_sale_manager' || product.name == 'group_sale_salesman_all_leads') {
             this.isShowSale = true;
-            this.sale();
+            this.isSale = true
+            this.isBuy = false
+            this.isEngineer = false
+            this.isMoney = false
           }
           if (product.name == 'group_purchase_user' || product.name == 'group_purchase_manager') {
             this.isShowPurchase = true;
-            this.buy();
-          }else{
-            this.engineer();
+            this.isBuy = true
+            this.isSale = false
+            this.isEngineer = false
+            this.isMoney = false
+          }
+          if(!this.isSale && !this.isBuy){
+            this.isEngineer = true
+            this.isBuy = false
+            this.isSale = false
+            this.isMoney = false
           }
           if (product.name == 'group_account_manager') {
             this.isShowZiJin = true;
@@ -113,7 +125,6 @@ export class WorkBenchPage {
           else if ((new RegExp("ber.robotime.com").test(res.result.res_data.user_ava))){
             this.company_type = "assets/img/B-header.png"
           }
-
          this.services.get_all_need_do(res.result.res_data.user_id,is_plus,this.isShowKucun,need_all).then(res => {
             console.log(res.result.res_data.bx)
             if (res.result && res.result.res_code == 1 && res.result.res_data) {
@@ -127,10 +138,28 @@ export class WorkBenchPage {
               this.kc_count = res.result.res_data.kc;
             }
           })
+          console.log('this.currenTab = '+this.currenTab+'this.isSale = '+this.isSale
+        +'this.isBuy = '+this.isBuy+'this.isEngineer = '+this.isEngineer)
+          switch(this.currenTab){
+            case 1:
+            this.sale()
+            break
+            case 2:
+            this.buy()
+            break
+            case 3:
+            this.engineer()
+            break
+            case 4:
+            this.money()
+            break
+          }
       });
   }
+ 
 //点击销售
   sale(){
+    this.currenTab = 1
     this.isSale = true
     this.isBuy = false
     this.isEngineer = false
@@ -138,6 +167,7 @@ export class WorkBenchPage {
   }
   //点击采购
   buy(){
+    this.currenTab = 2
     this.isSale = false
     this.isBuy = true
     this.isEngineer = false
@@ -145,6 +175,7 @@ export class WorkBenchPage {
   }
   //点击工程
   engineer(){
+    this.currenTab = 3
     this.isSale = false
     this.isBuy = false
     this.isEngineer = true
@@ -152,6 +183,7 @@ export class WorkBenchPage {
   }
   //财务
   money(){
+    this.currenTab = 4
     this.isSale = false
     this.isBuy = false
     this.isEngineer = false
