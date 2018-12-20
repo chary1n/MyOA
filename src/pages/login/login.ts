40991972d37364c63866f88f011b8be63d170d28
shangdinvxu <997399759@qq.com>
2018-12-20 13:57:07 +0800

    保存

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -283,9 +283,9 @@ export class LoginPage {
     this.isSelected3 = false;
     this.isSelected4 = false;
     this.chooseIndex = 0;
-    // HttpService.appUrl = "http://service.linkloving.net:8888/"
+    HttpService.appUrl = "http://service.linkloving.net:8888/"
     // HttpService.appUrl = "http://192.168.3.9:8081/"
-    HttpService.appUrl = "http://10.0.0.10:8111/"
+    // HttpService.appUrl = "http://10.0.0.10:8111/"
     this.reset();
     this.img1 = "assets/img/jiangsuruotai_clicked.png"
     this.password_src = "assets/img/S_password.png"
