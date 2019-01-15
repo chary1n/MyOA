b23ed081c8f0c2dd9e1138f0010d3ad038849452
shangdinvxu <997399759@qq.com>
2019-01-15 16:14:50 +0800

    提交

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,7 @@ export class MyApp {
       statusBar.styleDefault();
       statusBar.backgroundColorByHexString('#f8f8f8');
       splashScreen.hide();
-      this.checkCodePush()
+      // this.checkCodePush()
       this.jpush.initJpush();
       // storage.get('user')
       //   .then(res => {
@@ -41,7 +41,8 @@ export class MyApp {
       //   });
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      
+
+       
 
       
     });
